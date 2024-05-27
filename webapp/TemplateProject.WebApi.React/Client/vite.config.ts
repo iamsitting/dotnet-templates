9b42a3a0954c8c4bb4e14cb6b90178649d638fe9
Carlos <carlos@iamsitting.com>
2024-05-26 20:17:53 -0500

    build is working

--- webapp/TemplateProject.WebApi.React/Client/vite.config.ts
+++ webapp/TemplateProject.WebApi.React/Client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/_content/React",
+  base: "/_content/TemplateProject.WebApi.React",
   build: {
     outDir: "../wwwroot",
     emptyOutDir: true,
--- webapp/TemplateProject.WebApi.React/Extensions/Helpers.cs
+++ webapp/TemplateProject.WebApi.React/Extensions/Helpers.cs
@@ -3,7 +3,6 @@ using System.Text.Json;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Html;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,7 +11,8 @@ namespace TemplateProject.WebApi.React.Extensions;
 public static class Helpers
 {
     private const string AreaName = "React";
-    public const string ManifestPath = ".vite/manifest.json";
+    private const string ManifestPath = ".vite/manifest.json";
+    private const string ClassLibPath = "TemplateProject.WebApi.React";
     private static JsonDocument? _manifestDoc;
     
     public static void AddReactApp(this IServiceCollection services)
@@ -31,7 +31,7 @@ public static class Helpers
             var stringBuilder = new StringBuilder();
             foreach (var cssPath in cssValue.EnumerateArray())
             {
-                stringBuilder.AppendLine($"<link rel=\"stylesheet\" href=\"/_content/React/{cssPath.GetString()}\">");
+                stringBuilder.AppendLine($"<link rel=\"stylesheet\" href=\"/_content/{ClassLibPath}/{cssPath.GetString()}\">");
             }
             return new HtmlString(stringBuilder.ToString());
         }
@@ -44,7 +44,7 @@ public static class Helpers
         if (manifest.HasValue && manifest.Value.TryGetProperty(key, out JsonElement keyValue) && keyValue.TryGetProperty("file", out JsonElement fileValue) && fileValue.ValueKind == JsonValueKind.String)
         {
             var scriptPath = fileValue.GetString();
-            return new HtmlString($"<script type=\"module\" src=\"/_content/React/{scriptPath}\"></script>");
+            return new HtmlString($"<script type=\"module\" src=\"/_content/{ClassLibPath}/{scriptPath}\"></script>");
         }
         return new HtmlString($"<!-- Failed to render {key}-->");
     }
@@ -54,8 +54,8 @@ public static class Helpers
     {
         if (_manifestDoc != null) return _manifestDoc.RootElement;
         var manifestFilePath = hostingEnvironment.IsDevelopment()
-            ? Path.Combine(hostingEnvironment.WebRootPath, "../../TemplateProject.WebApi.React/wwwroot", ManifestPath)
-            : Path.Combine(hostingEnvironment.WebRootPath, "_content/React", "manifest.json");
+            ? Path.Combine(hostingEnvironment.WebRootPath, $"../../{ClassLibPath}/wwwroot", ManifestPath)
+            : Path.Combine(hostingEnvironment.WebRootPath, $"_content/{ClassLibPath}", "manifest.json");
 
         if (File.Exists(manifestFilePath))
         {
