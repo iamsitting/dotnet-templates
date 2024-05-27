6438caebda05c48a07e368f008706d7b022810f9
Carlos <carlos@iamsitting.com>
2024-05-26 20:49:50 -0500

    get hmr working

--- webapp/TemplateProject.WebApi.React/Extensions/Helpers.cs
+++ webapp/TemplateProject.WebApi.React/Extensions/Helpers.cs
@@ -1,6 +1,5 @@
 using System.Text;
 using System.Text.Json;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Html;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +12,7 @@ public static class Helpers
     private const string AreaName = "React";
     private const string ManifestPath = ".vite/manifest.json";
     private const string ClassLibPath = "TemplateProject.WebApi.React";
+    private const int DevPort = 5173;
     private static JsonDocument? _manifestDoc;
     
     public static void AddReactApp(this IServiceCollection services)
@@ -25,10 +25,15 @@ public static class Helpers
     
     public static IHtmlContent RenderStyles(this IWebHostEnvironment hostingEnvironment, string key)
     {
+        if (hostingEnvironment.IsDevelopment())
+        {
+            return new HtmlString($"<!-- Development: Nothing to render.-->");
+        }
         var manifest = GetManifestData(hostingEnvironment);
         if (manifest.HasValue && manifest.Value.TryGetProperty(key, out var keyValue) && keyValue.TryGetProperty("css", out JsonElement cssValue) && cssValue.ValueKind == JsonValueKind.Array)
         {
             var stringBuilder = new StringBuilder();
+            
             foreach (var cssPath in cssValue.EnumerateArray())
             {
                 stringBuilder.AppendLine($"<link rel=\"stylesheet\" href=\"/_content/{ClassLibPath}/{cssPath.GetString()}\">");
@@ -43,6 +48,25 @@ public static class Helpers
         var manifest = GetManifestData(hostingEnvironment);
         if (manifest.HasValue && manifest.Value.TryGetProperty(key, out JsonElement keyValue) && keyValue.TryGetProperty("file", out JsonElement fileValue) && fileValue.ValueKind == JsonValueKind.String)
         {
+            if (hostingEnvironment.IsDevelopment())
+            {
+                var stringBuilder = new StringBuilder();
+                stringBuilder.AppendLine(
+                    $$"""
+                    <script type="module">
+                      import RefreshRuntime from 'http://localhost:{{DevPort}}/_content/{{ClassLibPath}}/@react-refresh'
+                      RefreshRuntime.injectIntoGlobalHook(window)
+                      window.$RefreshReg$ = () => {}
+                      window.$RefreshSig$ = () => (type) => type
+                      window.__vite_plugin_react_preamble_installed__ = true
+                    </script>
+                    """);
+                stringBuilder.AppendLine($"""<script type="module" src="http://localhost:{DevPort}/_content/{ClassLibPath}/@vite/client"></script>""");
+                stringBuilder.AppendLine(
+                    $"""<script type="module" src="http://localhost:{DevPort}/_content/{ClassLibPath}/{key}"></script>""");
+                return new HtmlString(stringBuilder.ToString());
+            }
+
             var scriptPath = fileValue.GetString();
             return new HtmlString($"<script type=\"module\" src=\"/_content/{ClassLibPath}/{scriptPath}\"></script>");
         }
