ea62b218dc97964684367acca123a92d68f6030c
Carlos <carlos@iamsitting.com>
2024-06-04 09:35:35 -0500

    use shared state

--- webapp/TemplateProject.WebApi.React/Client/src/features/landing/Login.tsx
+++ webapp/TemplateProject.WebApi.React/Client/src/features/landing/Login.tsx
@@ -1,22 +1,22 @@
-import {init} from './state.ts'
+import {useLandingStore} from './store.ts'
 
 function Login() {
-    const state = init();
+    const store = useLandingStore();
 
     return (
         <>
             <div>
                 <label className="form-label">Username</label>
-                <input type="text" className="form-control" value={state.authFormUsername}
-                       onChange={(e) => state.setAuthFormUsername(e.target.value)}></input>
+                <input type="text" className="form-control" value={store.state.authForm.username}
+                       onChange={(e) => store.setAuthFormUsername(e.target.value)}></input>
             </div>
             <div>
                 <label className="form-label">Password</label>
-                <input type="password" className="form-control" value={state.authFormPassword}
-                       onChange={(e) => state.setAuthFormPassword(e.target.value)}></input>
+                <input type="password" className="form-control" value={store.state.authForm.password}
+                       onChange={(e) => store.setAuthFormPassword(e.target.value)}></input>
             </div>
             <div>
-                <button type="button" className="btn btn-primary btn-sm" onClick={() => state.submitLoginForm()}>Submit
+                <button type="button" className="btn btn-primary btn-sm" onClick={() => store.submitLoginForm()}>Submit
                 </button>
             </div>
         </>
--- webapp/TemplateProject.WebApi.React/Client/src/features/landing/Register.tsx
+++ webapp/TemplateProject.WebApi.React/Client/src/features/landing/Register.tsx
@@ -1,27 +1,27 @@
-import {init} from './state.ts'
+import {useLandingStore} from './store.ts'
 
 function Login() {
-    const state = init();
+    const store = useLandingStore();
 
     return (
         <>
             <div>
                 <label className="form-label">Username</label>
-                <input type="text" className="form-control" value={state.regFormUsername}
-                       onChange={(e) => state.setRegFormUsername(e.target.value)}></input>
+                <input type="text" className="form-control" value={store.state.regForm.username}
+                       onChange={(e) => store.setRegFormUsername(e.target.value)}></input>
             </div>
             <div>
                 <label className="form-label">Password</label>
-                <input type="password" className="form-control" value={state.regFormPassword}
-                       onChange={(e) => state.setRegFormPassword(e.target.value)}></input>
+                <input type="password" className="form-control" value={store.state.regForm.password}
+                       onChange={(e) => store.setRegFormPassword(e.target.value)}></input>
             </div>
             <div>
                 <label className="form-label">Confirm Password</label>
-                <input type="password" className="form-control" value={state.RegFormConfirmPassword}
-                       onChange={(e) => state.setRegFormConfirmPassword(e.target.value)}></input>
+                <input type="password" className="form-control" value={store.state.regForm.confirmPassword}
+                       onChange={(e) => store.setRegFormConfirmPassword(e.target.value)}></input>
             </div>
             <div>
-                <button type="button" className="btn btn-primary btn-sm" onClick={() => state.submitRegisterForm()}>Submit
+                <button type="button" className="btn btn-primary btn-sm" onClick={() => store.submitRegisterForm()}>Submit
                 </button>
             </div>
         </>
