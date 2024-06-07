e0c59f080677c39db755723fffa6247f11ad7ddd
Carlos <carlos@iamsitting.com>
2024-06-07 08:17:11 -0500

    new hydro project

--- webapp/TemplateProject.WebApi.Htmx/Areas/Htmx/Pages/Index.cshtml.cs
+++ webapp/TemplateProject.WebApi.Htmx/Areas/Htmx/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace TemplateProject.WebApi.Htmx.Areas.Htmx.Pages;
 
@@ -7,5 +9,15 @@ public class Index : PageModel
     public void OnGet()
     {
         
+    }
+
+    public IActionResult OnGetLoginForm()
+    {
+        return Partial("Landing/_LoginForm");
+    }
+
+    public IActionResult OnGetRegisterForm()
+    {
+        return Partial("Landing/_RegisterForm");
     }
 }
