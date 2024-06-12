73dfb4e00c99b172e9c3534cfc589134216f30f1
Carlos <carlos@iamsitting.com>
2024-06-12 08:49:34 -0500

    use package manager

---
+++ webapp/TemplateProject.WebApi.Htmx/Client/src/index.js
@@ -0,0 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import './layout.css';
+
+import htmx from 'htmx.org'
+
+function updateErrorDiv(htmlContent){
+    let errorDiv = document.querySelector("#error-msg");
+    errorDiv.insertAdjacentHTML('beforeend', htmlContent);
+    htmx.process(errorDiv);
+    errorDiv.scrollIntoView();
+}
+
+function getAlert(message){
+    return `
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <span>${message}</span>
+    <button type="button" class="btn-close" aria-label="Close" onclick="event.target.parentElement.remove()"></button>
+</div>`;
+}
+
+/* Global error handling */
+document.addEventListener("htmx:responseError", (e) => {
+    console.error(e.detail.error);
+    updateErrorDiv(e.detail.xhr.response)
+});
+
+/* Global network error */
+document.addEventListener("htmx:sendError", (e) => {
+    console.error(e.detail.elt);
+    updateErrorDiv(getAlert("Network error: Cannot reach server"));
+});
+
--- webapp/TemplateProject.WebApi.Htmx/Helpers.cs
+++ webapp/TemplateProject.WebApi.Htmx/Helpers.cs
@@ -1,5 +1,10 @@
+using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Html;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using TemplateProject.WebApi.Htmx.Areas.Htmx.Pages.Books;
 using TemplateProject.WebApi.Htmx.Areas.Htmx.Pages.Landing;
 
@@ -19,5 +24,55 @@ public static class Helpers
     {
         app.MapRazorPages();
         //app.MapFallbackToAreaPage("/Htmx/{*htmxAppPath}", "/Index", area: "Htmx");
+    }
+    
+    private const string ManifestPath = ".vite/manifest.json";
+    private const string ClassLibPath = "TemplateProject.WebApi.Htmx";
+    private static JsonDocument? _manifestDoc;
+    
+    public static IHtmlContent RenderStyles(this IWebHostEnvironment hostingEnvironment, string key)
+    {
+        var manifest = GetManifestData(hostingEnvironment);
+        if (manifest.HasValue && manifest.Value.TryGetProperty(key, out var keyValue) && keyValue.TryGetProperty("css", out JsonElement cssValue) && cssValue.ValueKind == JsonValueKind.Array)
+        {
+            var stringBuilder = new StringBuilder();
+            
+            foreach (var cssPath in cssValue.EnumerateArray())
+            {
+                stringBuilder.AppendLine($"<link rel=\"stylesheet\" href=\"/_content/{ClassLibPath}/{cssPath.GetString()}\">");
+            }
+            return new HtmlString(stringBuilder.ToString());
+        }
+        return new HtmlString($"<!-- Failed to render {key}-->");
+    }
+
+    public static IHtmlContent RenderScripts(this IWebHostEnvironment hostingEnvironment, string key)
+    {
+        var manifest = GetManifestData(hostingEnvironment);
+        if (manifest.HasValue && manifest.Value.TryGetProperty(key, out JsonElement keyValue) && keyValue.TryGetProperty("file", out JsonElement fileValue) && fileValue.ValueKind == JsonValueKind.String)
+        {
+            var scriptPath = fileValue.GetString();
+            return new HtmlString($"<script type=\"module\" src=\"/_content/{ClassLibPath}/{scriptPath}\"></script>");
+        }
+        return new HtmlString($"<!-- Failed to render {key}-->");
+    }
+
+
+    private static JsonElement? GetManifestData(IWebHostEnvironment hostingEnvironment)
+    {
+        if (_manifestDoc != null) return _manifestDoc.RootElement;
+        var manifestFilePath = hostingEnvironment.IsDevelopment()
+            ? Path.Combine(hostingEnvironment.WebRootPath, $"../../{ClassLibPath}/wwwroot", ManifestPath)
+            : Path.Combine(hostingEnvironment.WebRootPath, $"_content/{ClassLibPath}", "manifest.json");
+
+        if (File.Exists(manifestFilePath))
+        {
+            var manifestJson = File.ReadAllText(manifestFilePath);
+            _manifestDoc = JsonDocument.Parse(manifestJson);
+            var root = _manifestDoc.RootElement;
+            return root;
+        }
+
+        return null;
     }
 }
